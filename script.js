5cdbe381801932fd110246cbe88fbd6f9f7a3851
Kirill <kikochnev@gmail.com>
2015-10-10 15:23:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function mapClicked() {
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
+	
+	L.marker([51.5, -0.09]).addTo(map)
+		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    		.openPopup();
 }
 
 $(document).ready(function() {
