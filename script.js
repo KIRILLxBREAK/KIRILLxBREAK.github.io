15846cf8e47869081b6a5423b010cebb06550f29
Kirill <kikochnev@gmail.com>
2015-10-11 16:25:35 +0300

    Change default coordinate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function mapClicked() {
 	alert("Map!");
 	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map').setView([55.432, 37.654], 13);
+	var map = L.map('map').setView([41.23, 2.11], 13);
  
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
-	L.marker([55.432, 37.654]).addTo(map)
+	L.marker([41.23, 2.11]).addTo(map)
 		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     		.openPopup();
 }
